265fd823a935d2e7851516c36f37c668a9a456da
Sam Han <shan@8thlight.com>
2015-04-16 11:58:02 +0100

    adds degenerate implementation for getting scores for the guess

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,30 @@
 class Game
-  attr_reader :ai
+  attr_reader :ai, :current_correct, :current_exact
 
   WELCOME_MESSAGE = "Welcome to Mastermind!"
   CORRECT_COLOURS_PROMPT = "How many colours are correct?"
+  EXACT_COLOURS_PROMPT = "How many colours are in the exact position?"
 
   def initialize(output=$stdout, ai=Ai.new)
     @output = output
     @ai = ai
   end
+
   def start
     display(WELCOME_MESSAGE)
     display(ai.make_guess)
     display(CORRECT_COLOURS_PROMPT)
+    display(EXACT_COLOURS_PROMPT)
   end
+
+  def get_number_of_correct_colours
+    @current_correct = [0, 1, 2, 3, 4].sample
+  end
+
+  def get_number_of_exact_colours
+    @current_exact = [0, 1, 2, 3, 4].sample
+  end
+
   private
   def display(message)
     @output.puts(message)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,19 +9,53 @@ describe Game do
   let(:output) { StringIO.new }
   let(:fakeAi) { FakeAi.new }
   let(:game) { Game.new(output, ) }
+
   it "displays welcome message" do
     game.start
     expect(output.string).to start_with(Game::WELCOME_MESSAGE)
   end
+
   it "has Ai player" do
     expect(game.ai).to be
   end
+
   it "displays the guess from Ai" do
     game.start
     expect(output.string).to include(fakeAi.make_guess) 
   end
+
   it "displays prompt for number of correct colours guessed" do
     game.start
     expect(output.string).to include(Game::CORRECT_COLOURS_PROMPT)
   end
+
+  it "displays prompt for number of exact colours guessed" do
+    game.start
+    expect(output.string).to include(Game::EXACT_COLOURS_PROMPT)
+  end
+
+  it "gets number of correct matches" do
+    number_of_correct_guesses = game.get_number_of_correct_colours
+    expect(game.current_correct).to eq(number_of_correct_guesses)
+  end
+
+  it "gets number of exact matches" do
+    number_of_exact_colours = game.get_number_of_exact_colours
+    expect(game.current_exact).to eq(number_of_exact_colours)
+  end
+
+  describe "#get_number_of_correct_colours" do
+    it "returns a mark between 0 and 4" do
+      number_of_correct_guesses = game.get_number_of_correct_colours
+      expect([0, 1, 2, 3, 4]).to include(number_of_correct_guesses)
+    end
+  end
+
+  describe "#get_number_of_exact_colours" do
+    it "returns a mark between 0 and 4" do
+      number_of_exact_guesses = game.get_number_of_exact_colours
+      expect([0, 1, 2, 3, 4]).to include(number_of_exact_guesses)
+    end
+  end
+
 end
